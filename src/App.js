120ac84453f3a18d942dc7c44ac0445850bfa1f7
raghavagarwal701 <raghavagarwal701@gmail.com>
2023-09-22 15:23:48 +0530

    terms link fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import Form from './Form/Form';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Questionnere from './Questionnere/Questionnere';
-import Essential8Form from './Form/Form';
+// import Essential8Form from './Form/Form';
 import TermsAndConditions from './Form/TermsAndConditions';
 
 const App = () => {
@@ -18,7 +18,7 @@ const App = () => {
         <Routes>
           <Route exact path='/' element={<Form userData={userData} updateData={updateData}/>}/>
           <Route path='/Questionnare' element={<Questionnere userData={userData} updateData={updateData}/>}/>
-          <Route path="/" element={<Essential8Form />} />
+          {/* <Route path="/" element={<Essential8Form />} /> */}
           <Route path="/terms-and-conditions" element={<TermsAndConditions />} /> 
         </Routes>
       </BrowserRouter>
