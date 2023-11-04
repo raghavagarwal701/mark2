0a1526290152a1cf42eaf4322d9b089a6b20b4bb
raghavagarwal701 <raghavagarwal701@gmail.com>
2023-11-04 13:12:43 +0530

    backend to azure

--- src/Questionnere/Questionnere.js
+++ src/Questionnere/Questionnere.js
@@ -1684,7 +1684,7 @@ Disclaimer: The Essential 8 maturity report provided herewith by Cyber Ethos is 
     if (isQuestionnaireCompleted) {
       // eslint-disable-next-line
       axios
-        .post("https://formbackend-as4m.onrender.com/form/add", updatedData)
+        .post("https://essential-8backend.azurewebsites.net/form/add", updatedData)
         .then((res) => {
           const addedData = res.data;
           console.log(`POST: user is added`, addedData);
@@ -1692,7 +1692,7 @@ Disclaimer: The Essential 8 maturity report provided herewith by Cyber Ethos is 
         .catch((err) => {
           console.error(err);
         });
-      generatePDFReport();
+       generatePDFReport();
     }
   });
 
